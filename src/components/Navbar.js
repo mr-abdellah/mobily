75c419b9c07c22201005a558441ccf3c3be7bc62
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-08-13 17:17:36 +0100

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { NavLink, Link } from 'react-router-dom';
 import '../styles/navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
   <nav className="navbar">
@@ -9,8 +9,7 @@ const Navbar = () => (
       Mobily
     </Link>
     <ul className="nav-links">
-      <NavLink to="/home">Homepage</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
+      <NavLink to="/home">Home</NavLink>
       <NavLink to="/services">Services</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="faq">FAQ</NavLink>
@@ -19,7 +18,7 @@ const Navbar = () => (
         <FontAwesomeIcon icon={faSearch} />
       </NavLink>
       <NavLink id="about" to="/about">
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={faCartShopping} />
       </NavLink>
     </ul>
   </nav>
