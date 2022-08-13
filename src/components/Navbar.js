9be1a5e8c12a31df90fa1727d4983e60767eb74f
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-08-13 09:04:56 +0100

    Style navbar and add font awesome icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
   <nav className="navbar">
-    <NavLink id="logo" to="/">Mobily</NavLink>
+    <NavLink id="logo" to="/">
+      Mobily
+    </NavLink>
     <ul className="nav-links">
       <NavLink to="/categories">Categories</NavLink>
       <NavLink to="/services">Services</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="faq">FAQ</NavLink>
       <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/search">sear</NavLink>
-      <NavLink to="/menu">menu</NavLink>
+      <NavLink to="/search">
+        <FontAwesomeIcon icon={faSearch} />
+      </NavLink>
+      <NavLink to="/menu">
+        <FontAwesomeIcon icon={faBars} />
+      </NavLink>
     </ul>
   </nav>
 );
